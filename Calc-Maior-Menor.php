4d46c9987ab6107db21da04dc2e9c310a7786606
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-10-27 17:46:13 -0300

    Refatorando contador

---
+++ Calc-Maior-Menor.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    
+    $numerosEsc = array($_POST['valor1'],$_POST['valor2'],$_POST['valor3'],$_POST['valor4'],$_POST['valor5']);
+    $maiorNum = 0;
+
+    for($i = 0; $i < 5; $i++){
+        if(empty($numerosEsc[$i])){
+            echo "Digite um número";
+            return;
+        }
+        if($numerosEsc[$i] >= $maiorNum){
+            $maiorNum = $numerosEsc[$i];
+        }
+        else{
+            $menorNum = $numerosEsc[$i];
+        }
+    }
+
+    echo "O maior número é: $maiorNum <br>";
+    echo "O menor número é: $menorNum";
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checando maior e menor número - PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <form action="Calc-Maior-Menor.php" method="post">
+            <h3>Descubra o maior e menor número</h3>
+            <fieldset>
+                Digite o 1º número: <input type="number" name="valor1" required>
+            </fieldset>
+            <fieldset>
+                Digite o 2º número: <input type="number" name="valor2" required>
+            </fieldset>
+            <fieldset>
+                Digite o 3º número: <input type="number" name="valor3" required>
+            </fieldset>
+            <fieldset>
+                Digite o 4º número: <input type="number" name="valor4" required>
+            </fieldset>
+            <fieldset>
+                Digite o 5º número: <input type="number" name="valor5" required>
+            </fieldset>
+            <fieldset>
+                <button type="submit">Enviar</button>
+                <button type="reset">Limpar</button>
+            </fieldset>
+        </form>
+    </div>
+</body>
+
+</html>
